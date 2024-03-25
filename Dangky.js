a62d2a25d42220479ed050c8e3a83ba3cc0f0e98
CodeingOver <164869507+CodeingOver@users.noreply.github.com>
2024-03-25 19:09:28 +0700

    Add files via upload

---
+++ Dangky.js
@@ -0,0 +1,15 @@
+function getValue(id) {
+    return document.getElementById(id).value.trim();
+}
+function validate() {
+    let password = getValue('password');
+    let repassword = getValue('repassword');
+    let match = true;
+
+    if (password != repassword) {
+        alert('Mật khẩu không khớp');
+        match = false;
+    }
+    return match;
+}
+document.getElementById('loginform').onsubmit = validate;
